40e05aafb1199d1dde24b2832e5ee5ac906efefe
Lucas <104266998+lucas-twygz@users.noreply.github.com>
2024-09-27 01:10:24 -0300

    27/09/2024 REACT HOME

---
+++ sigma-games/src/App.js
@@ -0,0 +1,14 @@
+import logo from './components/imgHome/logo.png';
+import './App.css';
+import Home from './components/Home'
+
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
